9dcae8a90bd6eccafe2d5371a7250dc6304dcbd8
vn0202 <nghiavuxp0202@gmail.com>
2024-02-13 21:58:34 +0700

    khai xuan

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$isSuccess = false;
+function tryHard()
+{
+    return true;
+}
+while(!$isSuccess)
+{
+ $isSuccess = tryHard();
+}
+echo "Try, try hard and more. You are only failure when you give up.\n";
+echo "When you believe you can, you are the half way of success!";
+
